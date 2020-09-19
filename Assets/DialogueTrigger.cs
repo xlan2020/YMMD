ea03e7bcdc02932e4e4b1fd7a711e3a5ccef47e7
sherr-99 <995227013@qq.com>
2020-09-19 03:19:03 -0400

    Initializing the project
    - Created codes for basic dialogue system: trigger by clicking on NPC; click to continue; typewriter animation; speed-up;
    - Created codes for NPC image change switcher (currently changing with each new line of dialogue)
    - All images & layout are subjected to change.
    
    TODO:
    - dialogue backlog
    - image change with specific lines

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Triggers a conversation;
+public class DialogueTrigger : MonoBehaviour
+{
+    // Creates a dialogue object
+    public Dialogue dialogue;
+
+    public void TriggerDialogue ()
+    {
+        // find a dialogueManager object and run StartDialogue()
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
---
+++ Assets/ImageSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitcher : MonoBehaviour
+{
+    public Image npc;
+    public Sprite[] oriSprites;
+    private Queue<Sprite> sprites;
+
+    void Start()
+    {
+        // initialize q and store sprites in it
+        sprites = new Queue<Sprite>();
+        foreach (Sprite s in oriSprites)
+        {
+            sprites.Enqueue(s);
+        }
+    }
+
+    // switches images from the q
+    public void Switch()
+    {
+        if (sprites.Count != 0)
+        {
+            npc.sprite = sprites.Dequeue();
+        }
+        return;
+    }
+
+
+}
