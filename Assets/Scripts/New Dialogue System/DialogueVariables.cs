7fff7f0ee20a9277bbc65b55e0befc479213e221
HarveyLijh <43796904+HarveyLijh@users.noreply.github.com>
2022-02-04 17:23:26 -0800

    Fix Ink.UnityIntegration bug
    program can now successfully build

--- Assets/Scripts/New Dialogue System/DialogueVariables.cs
+++ Assets/Scripts/New Dialogue System/DialogueVariables.cs
@@ -9,18 +9,18 @@ public class DialogueVariables
     public Dictionary<string, Ink.Runtime.Object> variables { get; private set; }
 
 
-    public DialogueVariables(string globalFilePath)
+    public DialogueVariables(TextAsset loadGlobalsJSON)
     {
-        string inkFileContents = File.ReadAllText(globalFilePath);
-        Ink.Compiler compiler = new Ink.Compiler(inkFileContents);
-        Story globalVariblesStory = compiler.Compile();
+        // create the story
+        Story globalVariablesStory = new Story(loadGlobalsJSON.text);
 
+        // initialize the dictionary
         variables = new Dictionary<string, Ink.Runtime.Object>();
-        foreach (string name in globalVariblesStory.variablesState)
+        foreach (string name in globalVariablesStory.variablesState)
         {
-            Ink.Runtime.Object value = globalVariblesStory.variablesState.GetVariableWithName(name);
+            Ink.Runtime.Object value = globalVariablesStory.variablesState.GetVariableWithName(name);
             variables.Add(name, value);
-            Debug.Log("initialize global dialogue variable: " + name + "=" + value);
+            Debug.Log("Initialized global dialogue variable: " + name + " = " + value);
         }
     }
 
