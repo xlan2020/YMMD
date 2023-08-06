45e51c0b6684921fe5aead97e94f56bed41df0de
xlan <lanxinerrr@gmail.com>
2023-08-06 22:37:44 +0800

    Money UI Gradually increase
    - add an coroutine function to make the money increase/decrease gradually (add by 1 each time, interval 0.04f)
    - this achieves a scripted animation effect
    - need to update initial money for each scene later

--- Assets/Scripts/UI_Money.cs
+++ Assets/Scripts/UI_Money.cs
@@ -2,27 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 public class UI_Money : MonoBehaviour
 {
     private Money money;
     public Text moneyDisplay;
+    private float displayMoney;
+    private float displayMoneyTarget;
 
     public void SetMoney(Money money)
     {
         this.money = money;
         money.onMoneyChanged += Money_OnMoneyChanged;
-        RefreshMoneyDisplay();
     }
 
 
     private void Money_OnMoneyChanged(object sender, System.EventArgs e)
     {
-        RefreshMoneyDisplay();
+        StartCoroutine(changingMoneyDisplay(money.GetMoney()));
     }
 
-    public void RefreshMoneyDisplay()
+    private IEnumerator changingMoneyDisplay(float targetMoney)
     {
-        moneyDisplay.text = money.GetMoney().ToString();
-    }
+        // take consideration of the case if money is not an integer
+        // the display animation increase / decrease money by 1 
+        // if the the difference between the target and current display is less than 1
+        // increasing and decreasing might never meet the target
+        // so we are calculating the difference but not only increasing and decreasing
+        // difference is absolute, so actually <-1 or >1
+        while (displayMoney - targetMoney < -1f)
+        {
+            displayMoney++;
+            moneyDisplay.text = displayMoney.ToString();
+            yield return new WaitForSeconds(0.04f);    // animation interval
+        }
+        while (displayMoney - targetMoney > 1f)
+        {
+            displayMoney--;
+            moneyDisplay.text = displayMoney.ToString();
+            yield return new WaitForSeconds(0.04f);    // animation interval
+        }
+        // else: 
+        // -1f < displayMoney - targetMoney < 1f
+        displayMoney = targetMoney;
+        moneyDisplay.text = displayMoney.ToString();
 
+    }
 }
+
