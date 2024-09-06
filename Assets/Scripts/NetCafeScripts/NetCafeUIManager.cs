adc90c6e42aeeaf147001adb912e114a9d4063c7
sherr-99 <61575146+sherr-99@users.noreply.github.com>
2024-09-07 05:17:29 +0800

    updatedNetCafeUI
    - Auto create titles, detail pages according to scriptable objects
    - Scriptable object texts auto-load to textboxes
    - Ex: Underlining for titles when hovering above - only good for 1 line titles
    
    TBF:
    - Ex: gradient news bg scrollable

--- Assets/Scripts/NetCafeScripts/NetCafeUIManager.cs
+++ Assets/Scripts/NetCafeScripts/NetCafeUIManager.cs
@@ -6,23 +6,30 @@ public class NetCafeUIManager : MonoBehaviour
     [Header("Content Loaded")]
     [SerializeField] private NetCafeContentScriptableObject contentScriptableObject;
     private NewsDetailScriptableObject[] newsArray;
+    private NewsDetailScriptableObject newsHeadline;
     private PostScriptableObject[] postArray;
-    private NewsDetailScriptableObject newsheadline;
 
     [Header("UI Elements")]
     public GameObject netBar;
-    public GameObject newsHomePage; // 同时作为新闻标题容器
-    public GameObject forumHomePage; // 同时作为论坛标题容器
+    public GameObject newsHomePage;
+    public GameObject forumHomePage;
+    public GameObject newsTitlePrefab;   // 新闻标题按钮预制体
+    public GameObject forumTitlePrefab;  // 论坛标题按钮预制体
 
-    public GameObject forumTitlePrefab;
-    public GameObject newsTitlePrefab;
-
+    // 论坛和新闻的标题容器
     public Transform newsHomeTitles;
     public Transform forumHomeTitles;
 
-    // 论坛和新闻的详情页容器
-    public Transform forumDetailPagesContainer;
-    public Transform newsDetailPagesContainer;
+    // 新闻和论坛详情页的预制体
+    public GameObject newsDetailPagePrefab;
+    public GameObject forumDetailPagePrefab;
+
+    public Transform newsDetailPages;
+    public Transform forumDetailPages;
+
+    // headline 是一种特殊的新闻页，功能上和其他新闻一样，但在UI上做区分
+    public GameObject newsHeadLine;
+    public Transform headlineDetailPage;
 
     // 导航栏按钮
     public Button newsButton;
@@ -34,84 +41,34 @@ public class NetCafeUIManager : MonoBehaviour
     public Button shutdownConfirmButton;
     public Button shutdownCancelButton;
 
-    // 私有变量：存储动态获取的元素
-    private GameObject[] newsDetailPages;
-    private GameObject[] forumDetailPages;
-    private Button[] forumTitleButtons;
-    private Button[] newsTitleButtons;
-
+    [Header("Font Setter")]
+    public SherryFontSetter fontSetter;
 
     void Awake()
     {
+        // 从 ScriptableObject 中加载新闻和论坛数据
         newsArray = contentScriptableObject.newsArray;
-        newsheadline = contentScriptableObject.headline;
+        newsHeadline = contentScriptableObject.headline;
         postArray = contentScriptableObject.postArray;
     }
 
     void Start()
     {
-        // 动态获取论坛和新闻的标题按钮
-        forumTitleButtons = forumHomePage.GetComponentsInChildren<Button>();
-        newsTitleButtons = newsHomePage.GetComponentsInChildren<Button>();
-
-        // 动态获取论坛和新闻详情页
-        forumDetailPages = new GameObject[forumDetailPagesContainer.childCount];
-        for (int i = 0; i < forumDetailPagesContainer.childCount; i++)
-        {
-            forumDetailPages[i] = forumDetailPagesContainer.GetChild(i).gameObject;
-        }
-
-        newsDetailPages = new GameObject[newsDetailPagesContainer.childCount];
-        for (int i = 0; i < newsDetailPagesContainer.childCount; i++)
-        {
-            newsDetailPages[i] = newsDetailPagesContainer.GetChild(i).gameObject;
-        }
-
-        // 设置初始状态：显示新闻首页，隐藏其他页面
-        ShowNewsHomePage();
+        // 加载新闻和论坛标题
+        LoadNewsHomePage();
+        LoadForumHomePage();
 
         // 设置导航栏按钮的点击事件
         newsButton.onClick.AddListener(ShowNewsHomePage);
         forumButton.onClick.AddListener(ShowForumHomePage);
         shutdownButton.onClick.AddListener(ShowShutdownPopup);
 
-        // 设置论坛标题按钮的点击事件
-        for (int i = 0; i < forumTitleButtons.Length; i++)
-        {
-            int index = i; // 创建局部变量来存储当前索引，防止闭包问题
-            forumTitleButtons[i].onClick.AddListener(() => ShowForumDetailPage(index));
-        }
-
-        // 设置新闻标题按钮的点击事件
-        for (int i = 0; i < newsTitleButtons.Length; i++)
-        {
-            int index = i; // 同样，创建局部变量来存储当前索引
-            newsTitleButtons[i].onClick.AddListener(() => ShowNewsDetailPage(index));
-        }
-
-        // 设置论坛详情页返回按钮的点击事件
-        foreach (GameObject detailPage in forumDetailPages)
-        {
-            Button backButton = detailPage.GetComponentInChildren<Button>();
-            if (backButton != null)
-            {
-                backButton.onClick.AddListener(ShowForumHomePage);
-            }
-        }
-
-        // 设置新闻详情页返回按钮的点击事件
-        foreach (GameObject detailPage in newsDetailPages)
-        {
-            Button backButton = detailPage.GetComponentInChildren<Button>();
-            if (backButton != null)
-            {
-                backButton.onClick.AddListener(ShowNewsHomePage);
-            }
-        }
-
         // 设置关机弹窗按钮的点击事件
         shutdownConfirmButton.onClick.AddListener(ConfirmShutdown);
         shutdownCancelButton.onClick.AddListener(HideShutdownPopup);
+
+        // 显示初始状态
+        ShowNewsHomePage();
     }
 
     // 显示新闻首页
@@ -138,6 +95,28 @@ public class NetCafeUIManager : MonoBehaviour
         forumButton.interactable = false;
     }
 
+    // 隐藏所有详情页
+    void HideAllDetailPages()
+    {
+        // 销毁现有的详情页内容
+        foreach (Transform child in newsDetailPages)
+        {
+            Destroy(child.gameObject); // 销毁现有的内容
+        }
+
+        foreach (Transform child in forumDetailPages)
+        {
+            Destroy(child.gameObject); // 销毁现有的内容
+        }
+
+        foreach (Transform child in headlineDetailPage)
+        {
+            Destroy(child.gameObject); // 销毁现有的内容
+        }
+
+        HideShutdownPopup();
+    }
+
     // 显示关机弹窗
     void ShowShutdownPopup()
     {
@@ -156,127 +135,263 @@ public class NetCafeUIManager : MonoBehaviour
         netBar.SetActive(false);
     }
 
-    // 隐藏所有详情页
-    void HideAllDetailPages()
-    {
-        foreach (var page in forumDetailPages)
-            page.SetActive(false);
-
-        foreach (var page in newsDetailPages)
-            page.SetActive(false);
-
-        HideShutdownPopup();
-    }
-
-    // 显示指定的论坛详情页
-    public void ShowForumDetailPage(int index)
-    {
-        HideAllDetailPages();
-        if (index >= 0 && index < forumDetailPages.Length)
-        {
-            LoadForumContent(postArray[index], forumDetailPages[index]);
-            forumDetailPages[index].SetActive(true);
-        }
-    }
-
-    // 显示指定的新闻详情页
-    public void ShowNewsDetailPage(int index)
-    {
-        HideAllDetailPages();
-        if (index >= 0 && index < newsDetailPages.Length)
-        {
-            LoadNewsContent(newsArray[index], newsDetailPages[index]);
-            newsDetailPages[index].SetActive(true);
-
-            // 加载新闻内容
-        }
-    }
-
-    // 加载内容到对应的文本框
-    private void LoadNewsContent(NewsDetailScriptableObject content, GameObject detailPage)
-    {
-        // 在详情页中找到对应的 Text 组件
-        Text titleText = FindText(detailPage, "NewsTitle");
-        Text authorText = FindText(detailPage, "NewsAuthor");
-        Text dateText = FindText(detailPage, "NewsTime");
-        Text contentText = FindText(detailPage, "NewsText");
-
-        // 将内容加载到 Text 组件中
-        titleText.text = content.title;
-        authorText.text = content.author;
-        dateText.text = content.time;
-        contentText.text = content.content;
-    }
-
-    private void LoadForumContent(PostScriptableObject content, GameObject detailPage)
-    {
-        // 在详情页中找到对应的 Text 组件
-        Text titleText = FindText(detailPage, "Title");
-        Text contentText = FindText(detailPage, "PostContent");
-
-        // 将内容加载到 Text 组件中
-        titleText.text = content.title + "\n";
-        contentText.text = content.content + "\n";
-    }
-
-    private void LoadForumHomePage(PostScriptableObject content, GameObject homepage)
-    {
-        // 在详情页中找到对应的 Text 组件
-        Text titleText = FindText(homepage, "Title");
-        Text authorTimeText = FindText(homepage, "AuthorTime");
-
-        // 将内容加载到 Text 组件中
-        titleText.text = content.title + "\n";
-        authorTimeText.text = content.author + " / " + content.lastEditTime;
-    }
-
+    // 加载新闻首页标题
     private void LoadNewsHomePage()
     {
+        // 清空现有新闻标题按钮
         foreach (Transform child in newsHomeTitles.transform)
         {
             Destroy(child.gameObject);
         }
 
+        // 加载特殊新闻的 Headline
+        LoadNewsHeadline(newsHeadline, newsHeadLine);
+
+        // 遍历新闻数组，生成新闻标题按钮
         for (int i = 0; i < newsArray.Length; i++)
         {
-            // 动态生成按钮，显示新闻标题
-            GameObject titleButton = Instantiate(NewsTitlePrefab, newsHomePage.transform);
+            GameObject titleButton = Instantiate(newsTitlePrefab, newsHomeTitles.transform);
+
+            // 显式激活生成的对象
+            ActivateButtonAndComponents(titleButton);
+
             Button buttonComponent = titleButton.GetComponent<Button>();
-            Text buttonText = titleButton.GetComponentInChildren<Text>();
+            Text buttonText = FindText(titleButton, "Title");
 
-            // 设置按钮文本为新闻标题，并为按钮添加点击事件
-            buttonText.text = newsArray[i].title;
+            // 设置新闻标题文本
+            buttonText.text = newsArray[i].title + "\n";
 
-            // 将每个按钮的点击事件绑定到显示新闻详情页的方法上
-            int index = i; // 需要一个局部变量来存储当前索引，防止闭包问题
+            // 将点击事件绑定到新闻详情页的显示
+            int index = i; // 闭包问题解决
             buttonComponent.onClick.AddListener(() => ShowNewsDetailPage(index));
+
+            // 强制更新布局，确保生成的标题按钮立即生效
+            LayoutRebuilder.ForceRebuildLayoutImmediate(newsHomeTitles.GetComponent<RectTransform>());
         }
 
-        LoadNewsHeadline(newsheadline, newsHomePage);
-
-
+        // 再次更新整个 Canvas 确保布局完全更新
+        Canvas.ForceUpdateCanvases();
     }
 
+    private void LoadForumHomePage()
+    {
+        // 清空现有论坛标题按钮
+        foreach (Transform child in forumHomeTitles.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // 遍历论坛数组，生成论坛标题按钮
+        for (int i = 0; i < postArray.Length; i++)
+        {
+            GameObject titleButton = Instantiate(forumTitlePrefab, forumHomeTitles.transform);
+
+            // 显式激活生成的对象
+            ActivateButtonAndComponents(titleButton);
+
+            Button buttonComponent = titleButton.GetComponent<Button>();
+            Text buttonText = FindText(titleButton, "Title");
+            Text authorTime = FindText(titleButton, "AuthorTime");
+
+
+            // 设置论坛标题文本
+            buttonText.text = postArray[i].title + "\n";
+            authorTime.text = postArray[i].author + " / " + postArray[i].lastEditTime + "\n";
+
+            // 将点击事件绑定到论坛详情页的显示
+            int index = i; // 闭包问题解决
+            buttonComponent.onClick.AddListener(() => ShowForumDetailPage(index));
+
+            // 强制更新布局
+            LayoutRebuilder.ForceRebuildLayoutImmediate(forumHomeTitles.GetComponent<RectTransform>());
+        }
+
+        // 再次更新整个 Canvas 确保布局完全更新
+        Canvas.ForceUpdateCanvases();
+    }
+
+    // 加载新闻 Headline
     private void LoadNewsHeadline(NewsDetailScriptableObject content, GameObject headlinePage)
     {
-        Text headLine = FindText(headlinePage, "HeadlineTitle");
-        headLine.text = content.title + "\n";
+        Text headlineText = FindText(headlinePage, "HeadlineTitle");
+        headlineText.text = content.title + "\n";
+
+        Button headlineButton = headlinePage.GetComponentInChildren<Button>();
+        headlineButton.onClick.AddListener(ShowHeadlinePage);
+
+        // 确保所有组件都激活
+        ActivateButtonAndComponents(headlinePage);
+
+        // 强制更新布局，确保Headline布局正确
+        LayoutRebuilder.ForceRebuildLayoutImmediate(newsHomeTitles.GetComponent<RectTransform>());
+        Canvas.ForceUpdateCanvases();
     }
 
+    // 显式激活 `Button`、`Text` 组件及其 `SizeFitter` 和 `LayoutGroup` 组件
+    private void ActivateButtonAndComponents(GameObject buttonObj)
+    {
+        buttonObj.SetActive(true); // 激活按钮对象
+
+        // 激活 Button 的子组件
+        Button buttonComponent = buttonObj.GetComponent<Button>();
+        if (buttonComponent != null)
+        {
+            buttonComponent.enabled = true; // 确保按钮激活
+        }
+
+        ButtonHoverUnderline underline = buttonObj.GetComponent<ButtonHoverUnderline>();
+        if (underline != null)
+        {
+            underline.enabled = true;
+        }
+
+        // 激活所有子级 Text 组件
+        Text[] textComponents = buttonObj.GetComponentsInChildren<Text>(true);
+        if (textComponents != null)
+        {
+            foreach (Text t in textComponents) {
+                t.gameObject.SetActive(true);
+                t.enabled = true;
+                t.GetComponent<ContentSizeFitter>().enabled = true;
+            }
+            
+        }
+
+        // 激活 SizeFitter 和 LayoutGroup 组件
+        ContentSizeFitter sizeFitter = buttonObj.GetComponent<ContentSizeFitter>();
+        if (sizeFitter != null)
+        {
+            sizeFitter.enabled = true;
+        }
+
+        LayoutGroup layoutGroup = buttonObj.GetComponent<LayoutGroup>();
+        if (layoutGroup != null)
+        {
+            layoutGroup.enabled = true;
+        }
+    }
+
+    // 显示新闻详情页
+    public void ShowNewsDetailPage(int index)
+    {
+        HideAllDetailPages();
+        GameObject detailPage = Instantiate(newsDetailPagePrefab, newsDetailPages.transform);
+
+        // 显式激活生成的对象
+        detailPage.SetActive(true);
+
+        LoadNewsContent(newsArray[index], detailPage);
+
+        // 设置Back按钮的点击事件
+        SetBackButtonFunction(detailPage, ShowNewsHomePage);
+
+        // 应用字体设置到新生成的页面
+        if (fontSetter != null)
+        {
+            fontSetter.SetFontRecursively(detailPage); // 应用字体
+        }
+
+        // 强制更新布局，确保详情页显示正确
+        LayoutRebuilder.ForceRebuildLayoutImmediate(newsDetailPages.GetComponent<RectTransform>());
+        Canvas.ForceUpdateCanvases();
+    }
+
+    // 显示 Headline 的详情页
+    public void ShowHeadlinePage()
+    {
+        HideAllDetailPages();
+        GameObject detailPage = Instantiate(newsDetailPagePrefab, headlineDetailPage.transform);
+
+        // 显式激活生成的对象
+        detailPage.SetActive(true);
+
+        LoadNewsContent(newsHeadline, detailPage);
+
+        // 设置Back按钮的点击事件
+        SetBackButtonFunction(detailPage, ShowNewsHomePage);
+
+        // 强制更新布局，确保详情页显示正确
+        LayoutRebuilder.ForceRebuildLayoutImmediate(headlineDetailPage.GetComponent<RectTransform>());
+        Canvas.ForceUpdateCanvases();
+    }
+
+    // 显示论坛详情页
+    public void ShowForumDetailPage(int index)
+    {
+        HideAllDetailPages();
+        GameObject detailPage = Instantiate(forumDetailPagePrefab, forumDetailPages.transform);
+
+        // 显式激活生成的对象
+        detailPage.SetActive(true);
+
+        LoadForumContent(postArray[index], detailPage);
+
+        // 设置Back按钮的点击事件
+        SetBackButtonFunction(detailPage, ShowForumHomePage);
+
+        // 应用字体设置到新生成的页面
+        if (fontSetter != null)
+        {
+            fontSetter.SetFontRecursively(detailPage); // 应用字体
+        }
+
+        // 强制更新布局，确保详情页显示正确
+        LayoutRebuilder.ForceRebuildLayoutImmediate(forumDetailPages.GetComponent<RectTransform>());
+        Canvas.ForceUpdateCanvases();
+    }
+
+    // 设置Back按钮的点击事件
+    private void SetBackButtonFunction(GameObject detailPage, UnityEngine.Events.UnityAction backAction)
+    {
+        Button backButton = detailPage.GetComponentInChildren<Button>(true); // 假设Back按钮已经存在并且有Button组件
+        if (backButton != null)
+        {
+            backButton.onClick.RemoveAllListeners(); // 清除之前的监听器
+            backButton.onClick.AddListener(() =>
+            {
+                Destroy(detailPage); // 销毁当前详情页
+                backAction.Invoke(); // 返回到首页
+            });
+        }
+    }
+
+    // 加载新闻内容到详情页
+    private void LoadNewsContent(NewsDetailScriptableObject content, GameObject detailPage)
+    {
+        Text titleText = FindText(detailPage, "NewsTitle");
+        Text authorText = FindText(detailPage, "NewsAuthor");
+        Text dateText = FindText(detailPage, "NewsTime");
+        Text contentText = FindText(detailPage, "NewsText");
+
+        // 设置详情页内容
+        titleText.text = content.title + "\n";
+        authorText.text = content.author + "\n";
+        dateText.text = content.time + "\n";
+        contentText.text = content.content + "\n";
+    }
+
+    // 加载论坛内容到详情页
+    private void LoadForumContent(PostScriptableObject content, GameObject detailPage)
+    {
+        Text titleText = FindText(detailPage, "Title");
+        Text contentText = FindText(detailPage, "PostContent");
+
+        // 设置论坛内容
+        titleText.text = content.title + "\n";
+        contentText.text = content.content + "\n";
+    }
+
+    // 查找指定名称的 Text 组件
     private Text FindText(GameObject detailPage, string name)
     {
-        // 获取所有子对象中包含的 Text 组件
         Text[] allTextComponents = detailPage.GetComponentsInChildren<Text>(true);
-
-        // 遍历所有 Text 组件，找到名称匹配的对象
         foreach (Text textComponent in allTextComponents)
         {
             if (textComponent.gameObject.name == name)
             {
-                return textComponent; // 返回找到的 Text 组件
+                return textComponent;
             }
         }
-
         return null;
     }
 }
--- Assets/Scripts/NetCafeScripts/SherryFontSetter.cs
+++ Assets/Scripts/NetCafeScripts/SherryFontSetter.cs
@@ -7,6 +7,29 @@ public class SherryFontSetter : MonoBehaviour
 
     void Start()
     {
+        ApplyFontToAllText();
+    }
+
+    // 递归方法，用于设置Text组件的字体
+    public void SetFontRecursively(GameObject obj)
+    {
+        // 查找当前对象上的Text组件
+        Text uiText = obj.GetComponent<Text>();
+        if (uiText != null && targetFont != null)
+        {
+            uiText.font = targetFont;
+        }
+
+        // 递归处理子对象
+        foreach (Transform child in obj.transform)
+        {
+            SetFontRecursively(child.gameObject);
+        }
+    }
+
+    // 应用字体到场景中的所有Text组件
+    public void ApplyFontToAllText()
+    {
         if (targetFont != null)
         {
             // 遍历场景中的所有根对象
@@ -19,23 +42,6 @@ public class SherryFontSetter : MonoBehaviour
         else
         {
             Debug.LogWarning("请在Inspector中设置目标字体！");
-        }
-    }
-
-    // 递归方法，用于设置Text组件的字体
-    void SetFontRecursively(GameObject obj)
-    {
-        // 查找当前对象上的Text组件
-        Text uiText = obj.GetComponent<Text>();
-        if (uiText != null)
-        {
-            uiText.font = targetFont;
-        }
-
-        // 递归处理子对象
-        foreach (Transform child in obj.transform)
-        {
-            SetFontRecursively(child.gameObject);
         }
     }
 }
